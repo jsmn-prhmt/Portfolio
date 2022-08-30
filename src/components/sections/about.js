467f3333ef0cdfa8bbe3390b8c98cfbe2b2291d5
jsmn-prhmt <jsmnprh@gmail.com>
2022-08-30 10:37:52 +0200

    update

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -119,7 +119,7 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript', 'TypeScript', 'GraphQL', 'Node.js/Express', 'React', 'Next.js/Gatsby',
+  const skills = ['JavaScript', 'TypeScript', 'GraphQL', 'Node.js/Express', 'React', 'Next.js', 'Gatsby',
     'MongoDB', 'MySQL'];
 
   return (
